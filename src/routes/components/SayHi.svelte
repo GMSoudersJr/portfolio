ff1c9db3d0981427b7f678a0d94df999267251d4
GMSoudersJr <gmsoudersjr@gmail.com>
2023-08-07 17:45:41 +0900

    Changed wording to hello and changed display to block for smaller width devices.

--- src/routes/components/SayHi.svelte
+++ src/routes/components/SayHi.svelte
@@ -6,20 +6,26 @@
   id="sayHi"
   transition:fade|global={{ delay: 250, duration: 2500 }}
 >
-  <h1>Hi</h1>
+  <h1>Hello</h1>
   <h1>👋</h1>
 </div>
 
 <style>
   #sayHi {
     display: grid;
-    grid-auto-flow: column;
+    grid-template-columns: repeat(2, auto);
     gap: 2em;
-
   }
+
   h1 {
     font-size: 10em;
     color: var(--backgroundWhite);
     font-family: var(--emojiFontFamily);
+    text-align: center;
+  }
+  @media screen and (max-width: 48em) {
+    #sayHi {
+      display: block;
+    }
   }
 </style>
