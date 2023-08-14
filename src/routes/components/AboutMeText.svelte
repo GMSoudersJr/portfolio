e141737f6cf0b9b2f7a6b51e104ad561d48a4c53
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2023-08-14 10:45:47 +0900

    updated the meta description and reworded the about me text.

--- src/routes/components/AboutMeText.svelte
+++ src/routes/components/AboutMeText.svelte
@@ -4,50 +4,54 @@
 
 <div id="about-me-text">
   <p>
-    For over 15 years, I have been an educator and mentor in South Korea. My
-    passion for learning and internal motivation led me back to tech.  After
-    years of surfing the internet, I realized how little I actually understood
-    about how it works.  Even now, the more I learn, the more I know I don't
-    know.
+    I’ve been teaching and inspiring people in South Korea for more than a
+    decade and a half. But I never stopped being a student myself. I always
+    wanted to know more about the magic behind the web. So I decided to dive
+    into the world of tech and code. And boy, was it deep! The more I explored,
+    the more I realized how much there is to learn. And I love it! 😍
   </p>
   <p>
-    My coding journey started with freeCodeCamp.org learning HTML, CSS,
-    and Javascript to develop responsive web pages and applications.  After
-    completing the full-stack developer certification, I thought I was done.
-    Then I came upon Python and created my own report card comment generator
-    using my years of previous comments as data.  I then stumbled into Django
-    and React Native.  With those, I made a prototype of a delivery-sharing
-    app, but the founders could not raise any funding.  From there I went down
-    the rabbit hole of linux, bash, vim, tmux, config files, playing in the
-    terminal.  I bought a raspberry pi and got comfortable with working on
-    a remote system.  Then an opportunity came to put my knowledge to use for
-    a start-up that had funding.  I inherited a code base and had to re-learn
-    React.js, but I implemented all of the features they asked for and got rid
-    of most of the bugs.
+    My coding journey began on freeCodeCamp.org, where I acquired the essential
+    skills of web development: HTML, CSS, and Javascript. I applied these
+    skills to create responsive web pages and applications, and earned a
+    full-stack developer certification. But I was eager to learn more. I
+    encountered Python, and used it to develop a report card comment generator,
+    using my previous comments as data. I also experimented with Django and
+    React Native, and developed a prototype of a delivery-sharing app. However,
+    the app did not receive the funding we expected. Then I explored linux,
+    bash, vim, tmux, config files, and terminal commands. I purchased a
+    raspberry pi, and learned how to work on a remote system. Then I got an
+    opportunity to work for a start-up that had funding. I inherited a code
+    base and had to re-learn React.js, but I successfully implemented all the
+    features they requested and eliminated a multitude of the bugs.
   </p>
   <p>
-    I spend time coding, so myself and others can spend time doing other
-    things.  When I notice repetitive tasks that take little effort, but much
-    time to complete, my brain goes to work on ways for a computer to handle
-    them.  <a href="#projects">Projects</a> Tired of making PO Sheets manually?
-    I got you!  Tired of making invoices one by one?  I got you!  Tired of
-    filling out report cards one at a time?  I got you!
+    I code to automate tasks and save time for myself and others. I look for
+    patterns and opportunities to optimize processes that are tedious and
+    time-consuming. Whether it is creating PO Sheets, invoices, or reports,
+    I can design solutions that can handle them efficiently and accurately. I
+    enjoy using my skills and creativity to solve problems and improve
+    workflows. 🤝
   </p>
   <p>
     Besides sitting in front of a computer, you can find me running around my
-    neighborhood or at the local driving range.  I have recently fallen back in
+    neighborhood or grinding at my local driving range.  I have recently fallen back in
     love with running and found a new passion in golf.
   </p>
 </div>
 
 <style>
-  p {
+  p, ul li {
     max-width: 600px;
     color: var(--contentText);
     font-size: 18px;
     line-height: 27px;
     font-family: var(--contentTextFontFamily);
     text-wrap: balance;
+  }
+
+  ul {
+    margin-top: 0;
   }
 
   #about-me-text {
