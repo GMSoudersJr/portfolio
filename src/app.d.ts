10d371c267761ae0fae74a3c03f77c956115d7eb
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2023-09-21 12:28:38 +0900

    Add type interface for locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,10 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			requestsLeftForThisRateWindow: string|null;
+			secondsUntilWindowReset: string|null;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
