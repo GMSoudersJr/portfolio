742bd0f2b9a94670a4863b43114eb9e6e986a710
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-05-07 09:26:30 +0900

    Revise the about me section

--- src/routes/components/AboutMeText.svelte
+++ src/routes/components/AboutMeText.svelte
@@ -4,42 +4,28 @@
 
 <div id="about-me-text">
   <p>
-    For over 15 years, I have been teaching and inspiring people in South
-    Korea, but I have also been an avid learner myself. I have always been
-    interested in the web and its opportunities, so I decided to plunge into
-    the world of tech and code. And it has been a wonderful experience! The
-    more I experimented, the more I realized how much there is to create. And I
-    relish every aspect of it!
+    After graduating with a BS degree from Cornell Univeristy, I took a leap
+    and traveled half-way around the world. My journey stopped in South Korea,
+    where I've been teaching and inspiring others for over 15 years. During
+    that time, and my curiosity led me to dive into the world of tech. Starting
+    with HTML, CSS, and Javascript on freeCodeCamp.org, I quickly progressed,
+    earning a full-stack developer certification and venturing into Python for
+    personal projects.
   </p>
   <p>
-    My coding journey began on freeCodeCamp.org, where I acquired the essential
-    skills of web development: HTML, CSS, and Javascript. I applied these
-    skills to create responsive web pages and applications, and earned a
-    full-stack developer certification. But I was eager to learn more. I
-    encountered Python, and used it to develop a report card comment generator,
-    using my previous comments as data. I also experimented with Django and
-    React Native, and developed a prototype of a delivery-sharing app. However,
-    the app did not receive the funding we expected. Then I purchased a
-    raspberry pi, and learned how to work on a remote system. From there, I
-    explored linux, bash, vim, tmux, config files, and terminal commands. Then
-    I got an opportunity to work for a start-up that had funding. I inherited
-    a code base and had to re-learn React.js, but I successfully implemented
-    all the features they requested and eliminated a multitude of the bugs.
+    My coding journey expanded to include Django, React Native, and a prototype
+    for a delivery-sharing app, though it unfortunately didn't secure funding.
+    I've also dabbled with Raspberry Pi and Linux, enhancing my skills in bash,
+    vim, tmux, and terminal commands. This technical proficiency landed me a
+    role at a startup, where I revamped a codebase, added new features, and
+    debugged in React.js.
   </p>
   <p>
-    I code to automate tasks and save time for myself and others. I look for
-    patterns and opportunities to optimize processes that are tedious and
-    time-consuming. Whether it is creating PO Sheets, invoices, or reports,
-    I can design solutions that can handle them efficiently and accurately. I
-    enjoy using my skills and creativity to solve problems and improve
-    workflows. 🤝
-  </p>
-  <p>
-    My computer is my primary tool for coding, but I also have other interests
-    that keep me in shape and entertained. I have fun running around my
-    neighborhood and playing golf at the closest driving range. Running is a
-    hobby that I have rediscovered, and golf is a passion that I have newly
-    developed.
+    Coding for me is about automating tasks, saving time, and optimizing
+    tedious processes. I enjoy devising efficient, accurate solutions for things
+    like PO Sheets, invoices, and reports. Outside of coding, I keep active by
+    running and playing golf, reconnecting with running as a hobby and
+    discovering a passion for golf.
   </p>
 </div>
 
